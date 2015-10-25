62d7ea6dcd627c823e6e25225293f35c4a56a25c
yarmolchuk <yarmolchuk@gmail.com>
2015-10-25 22:46:36 +0200

    New sample project Reactive Cocoa plus Parse.com

---
+++ ReactivePlusParseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactivePlusParseDemo
+//
+//  Created by Dima on 25.10.15.
+//  Copyright © 2015 Axon Development Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReactivePlusParseDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactivePlusParseDemo
+//
+//  Created by Dima on 25.10.15.
+//  Copyright © 2015 Axon Development Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
